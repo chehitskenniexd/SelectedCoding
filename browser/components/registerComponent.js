367e7dec996726c11d30d00e837a7f47c023feda
Kennie M <Kennie M>
2016-12-17 04:08:33 -0500

    added general input fields gave First Name, Last Name, E-mail for submission form.

--- browser/components/registerComponent.js
+++ browser/components/registerComponent.js
@@ -10,7 +10,31 @@ export default class SignupComponent extends React.Component {
     render() {
         return (
             <div id="register-container">
-                Register Component
+                <div className="container" id="register-form-container"
+                    style={{ paddingTop: '5vh', paddingBottom: '5vh' }}>
+                    <div className="form-group row">
+                        <label className="col-xs-2 col-form-label">First Name</label>
+                        <div className="col-xs-10">
+                            <input className="form-control" type="text" placeholder="Kenneth" id="example-text-input" />
+                        </div>
+                    </div>
+
+                    <div className="form-group row">
+                        <label className="col-xs-2 col-form-label">Last Name</label>
+                        <div className="col-xs-10">
+                            <input className="form-control" type="text" placeholder="Moy" id="example-text-input" />
+                        </div>
+                    </div>
+
+                    <div className="form-group row">
+                        <label className="col-xs-2 col-form-label">Email</label>
+                        <div className="col-xs-10">
+                            <input className="form-control" type="text" placeholder="moykenneth91@gmail.com" id="example-text-input" />
+                        </div>
+                    </div>
+
+                    <button className="btn btn-outline-success float-md-right"type="button">Submit</button>
+                </div>
             </div>
         );
     }
