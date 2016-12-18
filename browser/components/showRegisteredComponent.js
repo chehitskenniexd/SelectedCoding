62ebe8a0ee550a2f8c6bc24cf26a14689d76cbc8
Kenneth Moy <moykenneth91@gmail.com>
2016-12-17 20:28:35 -0500

    added a progress bar for the show registered page

--- browser/components/showRegisteredComponent.js
+++ browser/components/showRegisteredComponent.js
@@ -12,20 +12,56 @@ export default class showRegisteredComponent extends React.Component {
   }
 
   componentWillMount() {
+    // get the registered users, then get the correct bounce types
     axios.get('/api/registered')
       .then(res => {
         this.setState({ registered: res.data });
       })
+      .then()
       .catch(err => console.log(err));
   }
 
   render() {
+    console.log(this.state.registered);
     return (
-      <div>Hello!
-                {
+      <div>
+        {
           this.state.registered
-            ? ''
-            : ''
+            ?
+            <div id="show-container">
+              <table className="table" id="registered-table">
+                <thead>
+                  <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                    <th>School</th>
+                    <th>Contacted</th>
+                    <th>Bounce</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {
+                    this.state.registered.map(registered => {
+                      return (<tr>
+                        <td>{registered.firstName}</td>
+                        <td>{registered.lastName}</td>
+                        <td>{registered.email}</td>
+                        <td>{registered.schoolName}</td>
+                        <td>{registered.contactedOn
+                          ? new Date(registered.contactedOn).toLocaleString()
+                          : ''}</td>
+                        <td>{registered.bounceType}</td>
+                      </tr>);
+                    })
+                  }
+                </tbody>
+              </table>
+            </div>
+
+            : <div>
+              <progress class="progress progress-striped progress-danger" value="100" max="100"></progress>
+            </div>
         }
       </div>
     )
